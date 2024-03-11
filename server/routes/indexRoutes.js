20ad6929e40959115d7c832608168eef78e3397d
yogendra09 <yogendrayadav168@gmail.com>
2024-03-11 18:33:26 +0530

    first commit

---
+++ server/routes/indexRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const mongoose = require("mongoose");
+const { home, userRegister, userLogin, currentUser, } = require("../controllers/indexController");
+const { isAuthenticated } = require("../middlewares/auth");
+//home
+router.get("/", home);
+
+//current user
+router.get("/user",isAuthenticated ,currentUser);
+
+//user register
+router.post("/register", userRegister);
+
+// user login
+router.post("/login", userLogin);
+
+
+
+
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv"
+dotenv.config();
+const app = express();
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server running on port ${process.env.PORT}`)
+})
