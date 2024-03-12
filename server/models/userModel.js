92d01f9ea7534ebd7f102b06517886ca3a3a4372
yogendra09 <yogendrayadav168@gmail.com>
2024-03-12 14:31:01 +0530

    schema Setup & error handler

---
+++ server/models/userModel.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+      name: {
+        type: String,
+      //   required: [true, "First Name is required"],
+      //   minLength: [4, "First Name must not contain more than 14 characters"],
+      },
+      contact: {
+        type: String,
+      //   required: [true, "Contact is required"],
+      //   maxLength: [10, "Contact must contain 10 characters"],
+      //   minLength: [10, "Contact must contain  10 characters"],
+      },
+      city: {
+        type: String,
+      //   required: [true, "First Name is required"],
+      //   minLength: [3, "city contain atleast 3 characters"],
+      },
+  
+      image: {
+        type: Object,
+        default: {
+          fileId: "",
+          url: "https://images.unsplash.com/photo-1682685797406-97f364419b4a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+        },
+      },
+      cart: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "product",
+        },
+      ],
+      email: {
+        type: String,
+        unique: true,
+        required: [true, "Email is required"],
+      //   match: [
+      //     /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      //     "Please fill a valid email address",
+      //   ],
+      },
+      password: {
+        type: String,
+        required:[true,"password is required"]
+      //   select: false,
+      //   maxLength: [15, "password should not exeed more than 15 charcters"],
+      //   minLength: [6, "password should have atleast 6 charcters"],
+        //    match:[/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,1024}$/,""]
+      },
+  
+      resetPasswordToken: {
+        type: String,
+        default: "0",
+      },
+    },
+    { timestamps: true }
+  );
+
+  const userModel = mongoose.model("user",userSchema);
+
+
+  export default userModel
--- server/server.js
+++ server/server.js
@@ -1,10 +1,28 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import logger from "morgan";
+import userRoutes from "./routes/userRoutes.js";
+import sellerRoutes from "./routes/sellerRoutes.js"
+import productRputes from "./routes/productRoutes.js" 
+import connectDB from "./config/db.js";
+import { notFound,errorHandler } from "./middlewares/errorMiddleware.js";
 dotenv.config();
+
 const app = express();
+app.use(logger('tiny'));
+connectDB();
 
 
 
+app.use("/user",userRoutes);
+app.use("/seller",sellerRoutes);
+app.use("/product",productRputes);
+
+
+app.get("/",(req,res)=> res.send("server is live"));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(process.env.PORT,()=>{
     console.log(`server running on port ${process.env.PORT}`)
