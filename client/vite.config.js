94b544f66a395ea22eb8ab848b4a921272f82321
yogendra singh <asyncyogendra@gmail.com>
2024-12-22 15:39:14 +0530

    added proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,4 +9,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://e-commerce-app-ewel.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
