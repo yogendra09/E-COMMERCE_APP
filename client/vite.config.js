5e19527f85b44b39ab7da5fc740d63ea6cc1db6e
yogendra singh <yogendrayadav09@yahoo.com>
2024-12-26 16:04:10 +0530

    updated vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src"),
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'https://e-commerce-app-ewel.onrender.com',
+      "/api": {
+        target: "https://e-commerce-app-ewel.onrender.com",
         changeOrigin: true,
         secure: false, // Use this if the backend server uses self-signed SSL certificates
-      },
+      }, // <-- Ensure this bracket is present
     },
   },
-})
+});
