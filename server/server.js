ff819339090580dc31ea8b65fb7133be042f1bfc
yogendra singh <yogendrayadav09@yahoo.com>
2024-12-27 16:59:09 +0530

    initialized socket.io

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const app = require('./app.js');
-const http = require('http')
+const http = require('http');
+const intializeSocket = require('./socket.js');
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
  
+require('./socket.js').intializeSocket(server);
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ server/socket.js
@@ -0,0 +1,28 @@
+const { Server } = require("socket.io");
+
+module.exports.intializeSocket = (server) => {
+    const io = new Server(server, {
+        cors: {
+        origin: "*", // Allow all origins
+        methods: ["GET", "POST"], // Allow only GET and POST requests
+        credentials: true, // Allow credentials (e.g., cookies)
+        },
+        allowEIO3: true,
+        transports: ["websocket"],
+      });
+
+    io.on('connection', (socket) => {
+        console.log('a user connected');
+
+
+        
+        socket.on('disconnect', () => {
+          console.log('user disconnected');
+        });
+      });
+
+    }
+
+
+
+    
